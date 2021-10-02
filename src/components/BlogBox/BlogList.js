0f2903ae2bff510f71501e4a433c660539218671
Fikri ihsan <mfikriihsan03@gmail.com>
2021-10-02 20:39:50 +0700

    add displayLimit functionality and change structure code

--- src/components/BlogBox/BlogList.js
+++ src/components/BlogBox/BlogList.js
@@ -1,49 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import BlogBox from "./BlogBox";
 // import { blogData } from "./blogData";
 
-const BlogList = ({ categoryParam }) => {
-  const [blogData, setBlogData] = useState([]);
-  const [clicked, setClicked] = useState(false);
-
-  useEffect(() => {
-    if (categoryParam === undefined) {
-      fetch("http://localhost:3001/blogData")
-        .then((res) => res.json())
-        .then((data) => setBlogData(data));
-    } else {
-      fetch(`http://localhost:3001/blogData/${categoryParam}`)
-        .then((res) => res.json())
-        .then((data) => setBlogData(data));
-    }
-  }, [categoryParam]);
+const BlogList = ({ categoryParam,blogData,clicked,setClicked }) => {
+  const [displayLimit, setDisplayLimit] = useState(10);
 
   if (clicked === true) {
     window.location.reload(false);
   }
+  // console.log(blogData.map().reverse())
   return (
     <>
       <Router>
-        {blogData.map((_, i) => {
-          return (
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              to={`/blog/${blogData[i].category}?id=${blogData[i].id}`}
-              key={[i]}
-              onClick={() => setClicked(true)}
-            >
-              <BlogBox
-                image={blogData[i].image}
-                title={blogData[i].title}
-                article={blogData[i].sub_title}
-                writer={blogData[i].author}
-                date={blogData[i].date}
-              />
-            </Link>
-          );
-        })}
+        {blogData
+          .slice(0, displayLimit)
+          .map((_, i) => {
+            return (
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to={`/blog/${blogData[i].category}?id=${blogData[i].id}`}
+                key={[i]}
+                onClick={() => setClicked(true)}
+              >
+                <BlogBox
+                  image={blogData[i].image}
+                  title={blogData[i].title}
+                  article={blogData[i].sub_title}
+                  writer={blogData[i].author}
+                  date={blogData[i].date}
+                />
+              </Link>
+            );
+          })
+          }
       </Router>
+      <button className="btn btn-info mx-auto" onClick={()=>setDisplayLimit(displayLimit+10)}> SHOW MORE BLOG </button>
     </>
   );
 };
