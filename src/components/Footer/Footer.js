8553f4cb244c0f75522d8e5ca1480b076f707ada
Fikri ihsan <mfikriihsan03@gmail.com>
2021-10-08 09:16:33 +0700

    fix link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,53 +4,21 @@ const Footer = () => {
   return (
     <section id="footer">
       <div className="container">
-        {/* <div className="row text-center text-xs-center text-sm-left text-md-left">
-          <div className="col-xs-12 col-sm-4 col-md-4">
-            <h5>Quick links</h5>
-            <ul className="list-unstyled quick-links">
-              <li>
-                <a href="https://www.fiverr.com/share/qb8D02">
-                  <i className="fa fa-angle-double-right"></i>Home
-                </a>
-              </li>
-              <li>
-                <a href="https://www.fiverr.com/share/qb8D02">
-                  <i className="fa fa-angle-double-right"></i>About
-                </a>
-              </li>
-              <li>
-                <a href="https://www.fiverr.com/share/qb8D02">
-                  <i className="fa fa-angle-double-right"></i>FAQ
-                </a>
-              </li>
-              <li>
-                <a href="https://www.fiverr.com/share/qb8D02">
-                  <i className="fa fa-angle-double-right"></i>Get Started
-                </a>
-              </li>
-              <li>
-                <a href="https://www.fiverr.com/share/qb8D02">
-                  <i className="fa fa-angle-double-right"></i>Videos
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div> */}
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
             <ul className="list-unstyled list-inline social text-center">
               <li className="list-inline-item">
-                <a href="https://www.facebook.com/Syiarsunnahtv-104237921497669">
+                <a href="https://www.facebook.com/Syiarsunnahtv-104237921497669" target="_blank" rel="noopener noreferrer">
                   <i className="fa fa-facebook"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://t.me/syiarsunnahtv">
+                <a href="https://t.me/syiarsunnahtv" target="_blank" rel="noopener noreferrer">
                   <i className="fa fa-telegram"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://instagram.com/syiarsunnah.tv?igshid=3xbfw1kr359l">
+                <a href="https://instagram.com/syiarsunnah.tv?igshid=3xbfw1kr359l" target="_blank" rel="noopener noreferrer">
                   <i className="fa fa-instagram"></i>
                 </a>
               </li>
@@ -68,7 +36,7 @@ const Footer = () => {
               © All right Reversed.
               <a
                 className="text-green ml-2"
-                href="https://www.sunlimetech.com"
+                href="https://github.com/Fikriihsan03"
                 target="_blank"
                 rel="noreferrer"
               >
